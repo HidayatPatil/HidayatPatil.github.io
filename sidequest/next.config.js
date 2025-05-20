304c979c2dd84826ddb80b1c5ed613b41512dd1c
HidayatPatil <hpatil2@my.bcit.ca>
2025-05-20 15:21:39 -0700

    testing

--- sidequest/next.config.js
+++ sidequest/next.config.js
@@ -4,8 +4,8 @@ const nextConfig = {
 	images: {
 		unoptimized: true, // Required if you're using the <Image> component
 	},
-	basePath: "/your-repo-name", // Change to your GitHub repo name
-	assetPrefix: "/your-repo-name", // Same as above
+	basePath: "/HidayatPatil.github.io", // Change to your GitHub repo name
+	assetPrefix: "/HidayatPatil.github.io", // Same as above
 };
 
 module.exports = nextConfig;
